c33a387bbc1c08370f5e11f01cea5facf55f9732
baneeishaque3 <baneeishaque3@gmail.com>
2021-06-13 16:23:55 +0000

    Conda Env. Debug

---
+++ reCreateCondaEnv.bash
@@ -0,0 +1,1 @@
+cd .. && conda activate base && pyenv global anaconda3-2020.11 && conda env remove -y -n RNN-SM && conda env create && pyenv global anaconda3-2020.11/envs/RNN-SM && conda activate RNN-SM
